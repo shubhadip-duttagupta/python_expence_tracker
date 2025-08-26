0771fc25877eeb494404e3469fda99836672fc54
shubhadip-duttagupta <duttaguptajoydeep@gmail.com>
2025-08-26 20:33:35 +0530

    adding new conditions in expence tracker

--- tracker.py
+++ tracker.py
@@ -31,16 +31,44 @@ def add_expense():
 
     print("Expense added successfully!")
 
+def view_expenses():
+    with open(FILENAME, mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
+
+def category_expenses():
+    total_expense = 0
+    category = input("Enter category name:")
+    with open(FILENAME, mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[1].lower() == category.lower():
+                total_expense = total_expense + float(row[2])
+
+    print(f"{category} category wise total expense: {total_expense}")       
+            
+
+
 # Simple menu
 while True:
     print("\nPersonal Expense Tracker")
     print("1. Add Expense")
-    print("2. Exit")
+    print("2. View Expenses")
+    print("3. Catategory wise Expenses")
+    print("4. Highest & Lowest Expense")
+    print("5. Exit")
     choice = input("Choose an option: ")
 
     if choice == '1':
         add_expense()
     elif choice == '2':
+        view_expenses()
+    elif choice == '3':
+        category_expenses()
+    elif choice == '4':
+        highlow_expense()
+    elif choice == '5':
         break
     else:
         print("Invalid choice. Try again.")
